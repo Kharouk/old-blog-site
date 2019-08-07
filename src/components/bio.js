33346cce9f387effd6677c65d4f79fe6477e2313
Alex Kharouk <akharoukpp@gmail.com>
2019-08-07 20:46:01 +0100

    fix pathing

--- src/components/bio.js
+++ src/components/bio.js
@@ -24,6 +24,8 @@ const Bio = () => {
       site {
         siteMetadata {
           author
+          # job
+          # jobTitles
           social {
             twitter
           }
@@ -37,7 +39,7 @@ const Bio = () => {
     <div
       style={{
         display: `flex`,
-        marginBottom: rhythm(2.5),
+        marginBottom: rhythm(2.5)
       }}
     >
       <Image
@@ -47,16 +49,17 @@ const Bio = () => {
           marginRight: rhythm(1 / 2),
           marginBottom: 0,
           minWidth: 50,
-          borderRadius: `100%`,
+          borderRadius: `100%`
         }}
         imgStyle={{
-          borderRadius: `50%`,
+          borderRadius: `50%`
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
-        {` `}
+        {/* Written by <strong>{author}</strong>. He likes dogs. And nature. And
+        doubting himself. Lastly, he works for {job} as a {jobTitle}.{` `} */}
+        Written by <strong>{author}</strong>. He needs a better desc from
+        graphql.
         <a href={`https://twitter.com/${social.twitter}`}>
           You should follow him on Twitter
         </a>
