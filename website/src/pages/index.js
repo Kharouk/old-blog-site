bc72d0ee80cfc174fa96a9108c4b18b686e273d0
Alex Kharouk <akharoukpp@gmail.com>
2019-08-07 19:58:28 +0100

    start dividing blog with portfolio (running into issues)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,7 +1,16 @@
 import React from "react"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
 
-const Index = () => {
-  return <div>Hello World</div>
+const Index = props => {
+  return (
+    <Layout location={props.location}>
+      <h1>
+        Site is under construction. Check out my <Link to="/blog">blog</Link> in
+        the meantime.
+      </h1>
+    </Layout>
+  )
 }
 
 export default Index
